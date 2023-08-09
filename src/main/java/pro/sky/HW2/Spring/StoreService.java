c702c478ef70931e4c0ec8e4d2dc7a5066a1d6ee
ZU <zshaihova@gmail.com>
2023-08-09 22:14:57 +0300

    начальный коммит

---
+++ src/main/java/pro/sky/HW2/Spring/StoreService.java
@@ -0,0 +1,7 @@
+package pro.sky.HW2.Spring;
+
+import java.util.Map;
+
+public interface StoreService {
+    public Map<Integer, String> basket();
+}
---
+++ src/main/java/pro/sky/HW2/Spring/StoreServiceImpl.java
@@ -0,0 +1,22 @@
+package pro.sky.HW2.Spring;
+
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+public class StoreServiceImpl implements StoreService {
+     private final Map<Integer, String> products = new HashMap<>();
+
+    public StoreServiceImpl() {
+        products.put(1, "apple");
+        products.put(2, "banana");
+        products.put(3, "cherry");
+    }
+
+    public Map<Integer, String> basket(){
+        return new HashMap<>(products);
+    }
+
+}
