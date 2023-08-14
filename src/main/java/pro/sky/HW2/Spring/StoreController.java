ab943e935c8f8168b77a60c2f0b6264815baa28b
ZU <zshaihova@gmail.com>
2023-08-14 16:55:44 +0300

    ДЗ выполнена

--- src/main/java/pro/sky/HW2/Spring/StoreController.java
+++ src/main/java/pro/sky/HW2/Spring/StoreController.java
@@ -4,7 +4,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import pro.sky.HW2.Spring.services.StoreService;
 import pro.sky.HW2.Spring.services.StoreServiceImpl;
 
 import java.util.List;
